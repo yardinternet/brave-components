17c3cbd77c536e0db7dde4b81d8880055da6578a
YvetteNikolov <yvettenikolov@gmail.com>
2025-02-26 10:27:01 +0100

    (feat): update readme and back-button html

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,5 +1,5 @@
-<div class="wp-block-theme-back-button wp-block-buttons {{ $align }} {{ $className }}">
-    <div class="is-style-arrow-back">
+<div class="wp-block-buttons {{ $align }} {{ $className }}">
+    <div class="wp-block-button is-style-back">
         <a class="wp-block-button__link" href="{{ $link }}">{{ $text }}</a>
     </div>
 </div>
