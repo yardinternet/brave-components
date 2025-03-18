d0765ef0f6d61292afbb901a1c9b174b5964b9a0
Yvette Nikolov <48315669+YvetteNikolov@users.noreply.github.com>
2025-03-18 16:22:40 +0100

    (chore): fix back-button character encoding

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,5 +1,5 @@
 <div class="wp-block-buttons {{ $align }} {{ $className }}">
     <div class="wp-block-button is-style-back">
-        <a class="wp-block-button__link" href="{{ $link }}">{{ $text }}</a>
+        <a class="wp-block-button__link" href="{{ $link }}">{!! $text !!}</a>
     </div>
 </div>
