a5e7b09af91743c5b9f31ea9f7141c7e9a606493
YvetteNikolov <yvettenikolov@gmail.com>
2026-02-16 10:50:35 +0100

    feat: add data-use-show attribute to dialog

--- resources/views/components/dialog/index.blade.php
+++ resources/views/components/dialog/index.blade.php
@@ -1,10 +1,11 @@
-@props(['id', 'ariaLabel'])
+@props(['id', 'ariaLabel', 'useShow' => false])
 
 <dialog
     {{ $attributes->merge([
-        'id' => $id, // Props because they are required
+        'id' => $id,
         'aria-label' => $ariaLabel,
         'class' => 'js-brave-dialog ' . $attributes->get('class'),
+		'data-use-show' => $useShow,
     ]) }}>
     {{ $slot }}
 </dialog>
