af389766447d64010133cab3e768fc114331625e
YvetteNikolov <yvettenikolov@gmail.com>
2026-03-13 14:26:22 +0100

    feat: refactor active to isActive, add activeClass and use button in stead of a when it has children

--- resources/views/components/nav/link.blade.php
+++ resources/views/components/nav/link.blade.php
@@ -1,45 +1,40 @@
 @props([
     'item' => null,
     'href' => null,
-    'active' => false,
+    'isActive' => false,
+	'activeClass' => null,
 	'hasChildren' => null,
 ])
 
 @php
-    if ($item && $href) {
-        throw new InvalidArgumentException(
-            'BraveNavLink: Do not pass both a Navi $item and $href.'
-        );
-    }
-
-    if (!$item && !$href) {
-        throw new InvalidArgumentException(
-            'BraveNavLink: Either a Navi $item or $href must be provided.'
-        );
-    }
-
 	$hasChildren = $hasChildren ?? !empty($item?->children);
 
-    $href = $hasChildren
-        ? '#'
-        : ($item->url ?? $href);
+    $href = $item->url ?? $href;
+    $label = $slot ?? $item->label ?? '';
 
-    $active = $active
+    $isActive = $isActive
     || ($item->active ?? false)
     || ($item->activeParent ?? false);
+
+    $baseAttributes = $attributes
+        ->class([
+            'brave-nav-link',
+			'brave-nav-link-has-children' => $hasChildren,
+            'brave-nav-link-is-active' => $isActive,
+            $activeClass => $isActive && $activeClass,
+            $item->classes ?? null,
+        ])
+        ->merge([
+            'aria-current' => $isActive ? 'page' : null,
+        ]);
 @endphp
 
-<a {{ $attributes
-    ->class([
-        'brave-nav-link',
-		'brave-nav-link-has-children' => $hasChildren,
-        'is-active' => $active,
-        $item->classes ?? null,
-    ])
-    ->merge([
-        'href' => $href,
-        'aria-current' => $active ? 'page' : null,
-    ])
-}}>
-    {{ $slot ?? $item->label ?? '' }}
-</a>
+@if ($hasChildren)
+    <button {{ $baseAttributes }}>
+        {{ $label }}
+    </button>
+@else
+    <a {{ $baseAttributes->merge(['href' => $href]) }}>
+        {{ $label }}
+    </a>
+@endif
