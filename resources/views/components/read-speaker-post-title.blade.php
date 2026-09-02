66dc6113180237f2bb612ab42ce52f849dabc5b7
Yannic van Veen <yannic.vanveen@yard.nl>
2026-09-02 09:36:39 +0200

    feat: add ReadSpeakerPostTitle component logic

--- resources/views/components/read-speaker-post-title.blade.php
+++ resources/views/components/read-speaker-post-title.blade.php
@@ -1,3 +1,16 @@
-<div class="brave-read-speaker-post-title">
+@php
+	/**
+	 * @var callable $showReadSpeaker
+	 * @var callable $showReadSpeakerAfter
+	 * @var callable $showReadSpeakerBefore
+	 * @var string $title
+	 */
+@endphp
+
+@if ($showReadSpeaker() && $showReadSpeakerBefore())
 	<x-brave-read-speaker />
-</div>
+@endif
+<h1 {{ $attributes->merge(['class' => 'brave-read-speaker-post-title']) }}>{!! $title !!}</h1>
+@if ($showReadSpeaker() && $showReadSpeakerAfter())
+	<x-brave-read-speaker />
+@endif
--- src/Components/ReadSpeakerPostTitle.php
+++ src/Components/ReadSpeakerPostTitle.php
@@ -7,11 +7,46 @@ namespace Yard\Brave\Components;
 use Illuminate\View\Component;
 use Illuminate\View\Factory;
 use Illuminate\View\View;
+use Yard\Brave\Hooks\ReadSpeaker as ReadSpeakerHook;
 
 class ReadSpeakerPostTitle extends Component
 {
+	public function __construct(
+		public string $title = '',
+	) {
+		$this->title = $this->resolveTitle($title);
+	}
+
 	public function render(): View|Factory|string
 	{
 		return view('brave::components.read-speaker-post-title');
+	}
+
+	public function showReadSpeaker(): bool
+	{
+		return 0 !== (int) (config('components.read_speaker.customer_id') ?? config('components.readSpeaker.customerId', 0));
+	}
+
+	public function showReadSpeakerBefore(): bool
+	{
+		return ReadSpeakerHook::POSITION_BEFORE === config('components.read_speaker.h1_position');
+	}
+
+	public function showReadSpeakerAfter(): bool
+	{
+		return ! $this->showReadSpeakerBefore();
+	}
+
+	private function resolveTitle(string $title): string
+	{
+		if (blank($title)) {
+			$title = get_the_title();
+		}
+
+		if (blank($title)) {
+			$title = 'Onbekende titel';
+		}
+
+		return wp_kses_post($title);
 	}
 }
